20a883978a4184fb3e4d75cb029652a8a6ff01fb
Elias Ablan <eliasablan93@gmail.com>
2025-05-31 16:34:11 +0200

    add suspense to wrap "/" route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,27 @@ import { Sidebar } from "@/components/Sidebar";
 import { Data } from "@/types/types";
 import Dashboard from "@/components/Dashboard";
 import { MobileMenuButton } from "@/components/MobileMenuButton";
+import { Suspense } from "react";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <DashboardProvider>
-      <div className="flex h-screen max-h-screen overflow-hidden">
-        <Sidebar
-          className="absolute w-full md:relative md:w-[400px] md:flex-none"
-          data={data as unknown as Data}
-        />
-        <Dashboard data={data as unknown as Data} />
-        <MobileMenuButton />
-      </div>
-    </DashboardProvider>
+    <Suspense
+      fallback={
+        <div className="flex h-screen items-center justify-center">
+          Loading...
+        </div>
+      }
+    >
+      <DashboardProvider>
+        <div className="flex h-screen max-h-screen overflow-hidden">
+          <Sidebar
+            className="absolute w-full md:relative md:w-[400px] md:flex-none"
+            data={data as unknown as Data}
+          />
+          <Dashboard data={data as unknown as Data} />
+          <MobileMenuButton />
+        </div>
+      </DashboardProvider>
+    </Suspense>
   );
 }
