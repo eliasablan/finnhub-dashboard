4183c83ccb8fd8db1f18a2b0becd573dd77cf0b4
Elias Ablan <eliasablan93@gmail.com>
2025-06-22 17:27:38 +0200

    remove border

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -101,7 +101,7 @@ function CompanyHeader() {
 
   if (!selectedCompanySymbol || !companyData) {
     return (
-      <div className="flex h-dvh items-center justify-center gap-8 border">
+      <div className="flex h-dvh items-center justify-center gap-8">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="48"
